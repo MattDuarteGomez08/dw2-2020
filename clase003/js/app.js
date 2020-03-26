cf633936618887b8f2398ff63079d20729aff79d
MattDuarteGomez08 <mati20gomez@gmail.com>
2020-03-25 20:19:02 -0400

    tarea clase003

---
+++ clase003/js/app.js
@@ -0,0 +1,30 @@
+var digito=5;
+console.log("Imprecion del digito 5");
+for( var f = 0 ; f < 11 ;f++){
+  resultante=digito*f;
+  console.log(resultante);
+}
+
+
+
+var digito2=5;
+var digito3=6;
+if (digito2<digito3)
+{
+  console.log("Tabla de multiplicar del digito Nº 5");
+  for(var f = 0 ; f < 11 ; f++){
+    resultante2=digito2*f;
+    console.log(resultante2);
+  }
+
+
+  console.log("Tabla de multiplicar del digito nº 6");
+    for(var f = 0 ; f < 11 ; f++){
+      resultante3=digito3*f;
+      console.log(resultante3);
+    }
+
+    
+}else{
+console.log("imposible procesar");
+}
